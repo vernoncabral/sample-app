8c6929f2fdc28134e125929a84b9f2ee3e97e599
vernoncabral <student-04-d6025cf312a3@qwiklabs.net>
2026-04-08 14:55:39 +0000

    added gcp code

--- main.go
+++ main.go
@@ -26,7 +26,7 @@ import (
 
 func main() {
 	http.HandleFunc("/blue", blueHandler)
-	http.HandleFunc("/red", redHandler)
+	// http.HandleFunc("/red", redHandler)
 	http.ListenAndServe(":8080", nil)
 }
 
